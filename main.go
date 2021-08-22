f89f12fdb04b73f713a45d46adedd387b3f0bfc8
imufiid <imufiid98@gmail.com>
2021-08-22 19:54:02 +0700

    defined handler for show users to JSON

--- main.go
+++ main.go
@@ -2,14 +2,47 @@ package main
 
 import (
 	"bwastartup/user"
-	"fmt"
+	_ "fmt"
 	"log"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
+	// connection to database
+	// dsn := "root:@tcp(127.0.0.1:3306)/learn_bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
+	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	// if err != nil {
+	// 	log.Fatal(err.Error())
+	// }
+
+	// fmt.Println("Connection to database is GOOD!!!")
+
+	// var users []user.User
+
+	// // get data users from db
+	// db.Find(&users)
+	
+	// // show user
+	// for i, user := range users {
+	// 	fmt.Println("ID : ",  user.ID)
+	// 	fmt.Println("Name : ",  user.Name)
+	// 	fmt.Println("Email : ",  user.Email)
+	// 	fmt.Println("Role : ",  user.Role)
+		
+	// 	if i != len(users)-1 {
+	// 		fmt.Println()
+	// 	}
+	// }
+
+}
+
+func handler(c *gin.Context)  {
+	
 	// connection to database
 	dsn := "root:@tcp(127.0.0.1:3306)/learn_bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
@@ -18,23 +51,12 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
-	fmt.Println("Connection to database is GOOD!!!")
-
 	var users []user.User
 
 	// get data users from db
 	db.Find(&users)
-	
-	// show user
-	for i, user := range users {
-		fmt.Println("ID : ",  user.ID)
-		fmt.Println("Name : ",  user.Name)
-		fmt.Println("Email : ",  user.Email)
-		fmt.Println("Role : ",  user.Role)
-		
-		if i != len(users)-1 {
-			fmt.Println()
-		}
-	}
+
+	// show users to JSON in web API
+	c.JSON(http.StatusOK, users)
 
 }
