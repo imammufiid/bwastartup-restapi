9dd5803a71c1cd12d560914ee02553631dfbc69c
imufiid <imufiid98@gmail.com>
2021-08-22 19:26:12 +0700

    cerate struct for represenation of field database

---
+++ user/entity.go
@@ -0,0 +1,12 @@
+package user
+
+import "time"
+
+// create struct for user entity
+// merepresentasikan field yang ada di database
+
+type User struct {
+	ID                                                          int
+	Name, Occupation, Email, PasswordHash, AvatarFileName, Role string
+	CreatedAt, UpdatedAt                                        time.Time
+}
