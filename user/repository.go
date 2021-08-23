13cddb929aa1a057275dc694e6f7507ac1491ad5
imufiid <imufiid98@gmail.com>
2021-08-23 20:33:37 +0700

    create contruct interface for Repository

---
+++ user/repository.go
@@ -0,0 +1,13 @@
+/*
+ * Created on Sun Aug 22 2021
+ *
+ *  Copyright (c) 2021 Imam Mufiid
+ */
+
+package user
+
+import "gorm.io/gorm"
+
+type Repository interface {
+	Save(user User) (User, error)
+}
