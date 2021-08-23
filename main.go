60ab05a5f725b9180fa6d9acef539be4a98231f3
imufiid <imufiid98@gmail.com>
2021-08-23 20:37:09 +0700

    init user repository

--- main.go
+++ main.go
@@ -4,53 +4,12 @@ import (
 	"bwastartup/user"
 	_ "fmt"
 	"log"
-	"net/http"
 
-	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
-	// connection to database
-	// dsn := "root:@tcp(127.0.0.1:3306)/learn_bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
-	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
-	// if err != nil {
-	// 	log.Fatal(err.Error())
-	// }
-
-	// fmt.Println("Connection to database is GOOD!!!")
-
-	// var users []user.User
-
-	// // get data users from db
-	// db.Find(&users)
-	
-	// // show user
-	// for i, user := range users {
-	// 	fmt.Println("ID : ",  user.ID)
-	// 	fmt.Println("Name : ",  user.Name)
-	// 	fmt.Println("Email : ",  user.Email)
-	// 	fmt.Println("Role : ",  user.Role)
-		
-	// 	if i != len(users)-1 {
-	// 		fmt.Println()
-	// 	}
-	// }
-
-	// routers
-	router := gin.Default()
-	// define route
-	router.GET("/users", handler)
-
-	// run router
-	router.Run()
-
-}
-
-func handler(c *gin.Context)  {
-	
 	// connection to database
 	dsn := "root:@tcp(127.0.0.1:3306)/learn_bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
@@ -59,12 +18,7 @@ func handler(c *gin.Context)  {
 		log.Fatal(err.Error())
 	}
 
-	var users []user.User
-
-	// get data users from db
-	db.Find(&users)
-
-	// show users to JSON in web API
-	c.JSON(http.StatusOK, users)
+	// init user repository
+	userRepo := user.Instance(db)
 
 }
