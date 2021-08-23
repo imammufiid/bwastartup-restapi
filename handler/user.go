6db11295e75abe72d220e231d0f298e857006a2e
imufiid <imufiid98@gmail.com>
2021-08-24 05:03:15 +0700

    create struct with dependency user service

---
+++ handler/user.go
@@ -0,0 +1,13 @@
+/*
+ * Created on Sun Aug 22 2021
+ *
+ *  Copyright (c) 2021 Imam Mufiid
+ */
+
+package handler
+
+import "bwastartup/user"
+
+type userHandler struct {
+	userService user.Service
+}
