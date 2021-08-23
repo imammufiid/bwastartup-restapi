15b3b6c77e39c21c933a3004ce99fda713dc5a7a
imufiid <imufiid98@gmail.com>
2021-08-24 05:16:52 +0700

    get input from user and bind to JSON

--- handler/user.go
+++ handler/user.go
@@ -6,7 +6,12 @@
 
 package handler
 
-import "bwastartup/user"
+import (
+	"bwastartup/user"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 type userHandler struct {
 	userService user.Service
@@ -14,4 +19,14 @@ type userHandler struct {
 
 func InstanceUserHandler(userService user.Service) *userHandler {
 	return &userHandler{userService: userService}
+}
+
+func (h *userHandler) RegisterUser(c *gin.Context)  {
+	// 1. get input from user to JSON
+	var input user.RegisterInput
+	// bind to JSON
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, nil)
+	}
 }
