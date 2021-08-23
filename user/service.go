f8bf5e738bf7f0d6905eac7ecd0a684141ae0a61
imufiid <imufiid98@gmail.com>
2021-08-24 04:12:23 +0700

    mapping input to entity

--- user/service.go
+++ user/service.go
@@ -16,18 +16,23 @@ type service struct {
 	repository Repository // dependency repository
 }
 
-func InstanceService(repository Repository) *service  {
+func InstanceService(repository Repository) *service {
 	return &service{repository: repository}
 }
 
 // implemented interface Service
 func (s *service) RegisterUser(input RegisterInput) (User, error) {
 	var user User = User{}
-	
+
 	// hasing password
 	passHash, err := bcrypt.GenerateFromPassword([]byte(input.Password), bcrypt.MinCost)
 
 	if err != nil {
 		return user, err
 	}
+	// 1. mapping from user input to user entity
+	user.Name = input.Name
+	user.Email = input.Email
+	user.Occupation = input.Occupation
+	user.PasswordHash = string(passHash)
 }
