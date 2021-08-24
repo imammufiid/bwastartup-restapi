3d5c89c7d51c751b0cb442fd3085307100e8fdd3
imufiid <imufiid98@gmail.com>
2021-08-24 19:27:26 +0700

    change key of object response if ype json

--- helper/helper.go
+++ helper/helper.go
@@ -1,26 +1,26 @@
 package helper
 
-// 1. create object response 
+// 1. create object response
 type Response struct {
-	Meta Meta
-	Data interface{} // why interface{}? bcoz value of the data can change
+	Meta Meta        `json:"meta"`
+	Data interface{} `json:"data"` // why interface{}? bcoz value of the data can change
 }
 
 type Meta struct {
-	Message string
-	Code    int
-	Status  string
+	Message string `json:"message"`
+	Code    int    `json:"code"`
+	Status  string `json:"status"`
 }
 
 // 2. Mapping value of response from handler
 func ApiResponse(message string, code int, status string, data interface{}) Response {
-	meta := Meta {
+	meta := Meta{
 		Message: message,
-		Code: code,
-		Status: status,
+		Code:    code,
+		Status:  status,
 	}
 
-	jsonResponse := Response {
+	jsonResponse := Response{
 		Meta: meta,
 		Data: data,
 	}
