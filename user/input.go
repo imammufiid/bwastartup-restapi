7146cadc16494d8d34b2f6f5d58cc01a3b4e5a85
imufiid <imufiid98@gmail.com>
2021-08-25 20:14:59 +0700

    add validation rule into each field

--- user/input.go
+++ user/input.go
@@ -8,10 +8,10 @@ package user
 
 // struct for mapping input from user
 type RegisterInput struct {
-	Name       string
-	Occupation string
-	Email      string
-	Password   string
+	Name       string `json:"name" binding:"required"`
+	Occupation string `json:"occupation" binding:"required"`
+	Email      string `json:"email" binding:"required,email"`
+	Password   string `json:"password" binding:"required"`
 }
 
 type LoginInput struct {
