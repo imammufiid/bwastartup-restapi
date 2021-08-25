977c5bcde84112de18bffbd6671a4ef5c808df9f
imufiid <imufiid98@gmail.com>
2021-08-26 04:08:46 +0700

    handle login user into handler

--- handler/user.go
+++ handler/user.go
@@ -71,3 +71,49 @@ func (h *userHandler) RegisterUser(c *gin.Context)  {
 
 	c.JSON(http.StatusCreated, response)
 }
+
+func (h *userHandler) Login(c *gin.Context) {
+	var input user.LoginInput
+
+	// bind input to JSON
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		// map to response
+		errorMessage := gin.H{"errors": errors}
+		// create error handling response
+		response := helper.ApiResponse(
+			"Login Failed",
+			http.StatusUnprocessableEntity,
+			"error",
+			errorMessage,
+		)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	// logged in process
+	loggedIn, err := h.userService.Login(input)
+	if err != nil {
+		errorMessage := gin.H{"errors": err.Error()}
+		// create error handling response
+		response := helper.ApiResponse(
+			"Login Failed",
+			http.StatusUnprocessableEntity,
+			"error",
+			errorMessage,
+		)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	// mapping to format user response
+	responseUser := user.FormatUser(loggedIn, "token123")
+	response := helper.ApiResponse(
+		"Successfuly loggedin",
+		http.StatusOK,
+		"success",
+		responseUser,
+	)
+	c.JSON(http.StatusOK, response)
+}
