c82eb32e26bd5a9a02a5fdd1c9d960ddc26998b2
imufiid <imufiid98@gmail.com>
2021-08-29 04:46:32 +0700

    create contruct and implemented find by ID

--- user/repository.go
+++ user/repository.go
@@ -11,6 +11,7 @@ import "gorm.io/gorm"
 type Repository interface {
 	Save(user User) (User, error)
 	FindByEmail(email string) (User, error)
+	FindByID(ID int) (User, error)
 }
 
 type repository struct {
@@ -37,6 +38,19 @@ func (r *repository) FindByEmail(email string) (User, error) {
 	
 	// find by email query
 	err := r.db.Where("email = ?", email).Find(&user).Error
+
+	if err != nil {
+		return user, err
+	}
+
+	return user, nil
+}
+
+func (r *repository) FindByID(ID int) (User, error) {
+	var user User
+	
+	// find by id query
+	err := r.db.Where("id = ?", ID).Find(&user).Error
 
 	if err != nil {
 		return user, err
