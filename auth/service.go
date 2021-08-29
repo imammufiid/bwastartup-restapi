82e322063aaff77ebd6d0ad7680acb2550a4491b
imufiid <imufiid98@gmail.com>
2021-08-29 11:03:20 +0700

    implemented generate jwt token

--- auth/service.go
+++ auth/service.go
@@ -1,5 +1,7 @@
 package auth
 
+import "github.com/dgrijalva/jwt-go"
+
 type Service interface {
 	GenerateToken(userID int) (string, error)
 }
@@ -7,6 +9,21 @@ type Service interface {
 type jwtService struct {
 }
 
+var SECRET_KEY = []byte("bWASt4rTUP_s3CREt_K3y")
+
 func (s *jwtService) GenerateToken(userID int) (string, error) {
-	
+	// generate payload jwt
+	claim := jwt.MapClaims{}
+	claim["user_id"] = userID // insert payload data
+
+	// generate jwt algoritm
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
+
+	// generate secret key
+	signedToken, err := token.SignedString(SECRET_KEY)
+	if err != nil {
+		return signedToken, err
+	}
+
+	return signedToken, nil
 }
