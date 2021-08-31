fbb596759d4ff34e8ee7f981a4c4f26c7de8e399
imufiid <imufiid98@gmail.com>
2021-08-31 14:04:08 +0700

    contruct campaign repository

--- campaign/repository.go
+++ campaign/repository.go
@@ -1,1 +1,16 @@
 package campaign
+
+import "gorm.io/gorm"
+
+type Repository interface {
+	FindAll() ([]Campaign, error)
+	FindByUserID(userID int) ([]Campaign, error)
+}
+
+type repository struct {
+	db *gorm.DB
+}
+
+func InstanceRepository(db *gorm.DB) *repository {
+	return &repository{db: db}
+}
