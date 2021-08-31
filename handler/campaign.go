f27bd3895e6c316f304cb0b2818366f9b6cacdb8
imufiid <imufiid98@gmail.com>
2021-08-31 14:46:06 +0700

    create construct campaign handler

--- handler/campaign.go
+++ handler/campaign.go
@@ -1,1 +1,14 @@
 package handler
+
+import "bwastartup/campaign"
+
+type CampaignHandler interface {
+}
+
+type campaignHandler struct {
+	campaignService campaign.Service
+}
+
+func InstanceCampaignHandler(campaignService campaign.Service) *campaignHandler {
+	return &campaignHandler{campaignService: campaignService}
+}
