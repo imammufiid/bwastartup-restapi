0031baa73cd6d86a46663ba2b67a31d31d0e22c3
imufiid <imufiid98@gmail.com>
2021-08-31 15:45:55 +0700

    handler get campaigns

--- handler/campaign.go
+++ handler/campaign.go
@@ -1,8 +1,16 @@
 package handler
 
-import "bwastartup/campaign"
+import (
+	"bwastartup/campaign"
+	"bwastartup/helper"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
 
 type CampaignHandler interface {
+	GetCampaigns(c *gin.Context)
 }
 
 type campaignHandler struct {
@@ -11,4 +19,31 @@ type campaignHandler struct {
 
 func InstanceCampaignHandler(campaignService campaign.Service) *campaignHandler {
 	return &campaignHandler{campaignService: campaignService}
+}
+
+func (h *campaignHandler) GetCampaigns(c *gin.Context) {
+	// get query param user_id
+	userID, _ := strconv.Atoi(c.Query("user_id"))
+	campaigns, err := h.campaignService.GetCampaigns(userID)
+	if err != nil {
+		// map to response
+		errorMessage := gin.H{"errors": err.Error()}
+		// create error handling response
+		response := helper.ApiResponse(
+			"Error to get campaigns",
+			http.StatusBadRequest,
+			"error",
+			errorMessage,
+		)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.ApiResponse(
+		"List of Campaigns",
+		http.StatusOK,
+		"success",
+		campaigns,
+	)
+	c.JSON(http.StatusOK, response)
 }
