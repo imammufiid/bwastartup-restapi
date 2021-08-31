306915ee5224eaa9365f1c28bdb517e7625ac192
imufiid <imufiid98@gmail.com>
2021-08-31 14:34:48 +0700

    init contruct campaign service

--- campaign/service.go
+++ campaign/service.go
@@ -1,1 +1,17 @@
 package campaign
+
+type Service interface {
+	GetCampaigns(userID int) ([]Campaign, error)
+}
+
+type service struct {
+	repository Repository
+}
+
+func InstanceService(repository Repository) *service {
+	return &service{repository: repository}
+}
+
+func (s *service) GetCampaigns(userID int) ([]Campaign, error) {
+
+}
