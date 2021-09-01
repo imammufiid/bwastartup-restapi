f276858fbc6e88c025755e4dbee0502e3be83c4a
imufiid <imufiid98@gmail.com>
2021-09-02 04:34:44 +0700

    query find by id campaign

--- campaign/repository.go
+++ campaign/repository.go
@@ -5,6 +5,7 @@ import "gorm.io/gorm"
 type Repository interface {
 	FindAll() ([]Campaign, error)
 	FindByUserID(userID int) ([]Campaign, error)
+	FindByID(campaignID int) (Campaign, error)
 }
 
 type repository struct {
@@ -32,3 +33,14 @@ func (r *repository) FindByUserID(userID int) ([]Campaign, error) {
 	}
 	return campaigns, nil
 }
+
+func (r *repository) FindByID(campaignID int) (Campaign, error) {
+	var campaign Campaign
+	err := r.db.Where("id = ?", campaignID).Preload("CampaignImages").Preload("User").Find(&campaign).Error
+	if err != nil {
+		return campaign, err
+	}
+	return campaign, nil
+}
+
+
