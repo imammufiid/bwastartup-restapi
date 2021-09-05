7dfe4f3fd6bf7c11f24552ef6b3107c9ac9a70b1
imufiid <imufiid98@gmail.com>
2021-09-05 19:13:26 +0700

    create repo, get by campaign id

---
+++ transaction/repository.go
@@ -0,0 +1,25 @@
+package transaction
+
+import "gorm.io/gorm"
+
+type Repository interface {
+	GetByCampaignID(campaignID int) ([]Transaction, error)
+}
+
+type repository struct {
+	db *gorm.DB
+}
+
+func InstanceRepository(db *gorm.DB) *repository {
+	return &repository{db: db}
+}
+
+func (r *repository) GetByCampaignID(campaignID int) ([]Transaction, error) {
+	var transactions []Transaction
+
+	err := r.db.Preload("User").Where("campaign_id = ?", campaignID).Find(&transactions).Error
+	if err != nil {
+		return transactions, err
+	}
+	return transactions, nil
+}
