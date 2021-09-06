967bff23a699781e01e85bc3485ab46c32b9f9bc
imufiid <imufiid98@gmail.com>
2021-09-06 18:49:44 +0700

    add authorization

--- transaction/service.go
+++ transaction/service.go
@@ -1,19 +1,37 @@
 package transaction
 
+import (
+	"bwastartup/campaign"
+	"errors"
+)
+
 type Service interface {
 	GetTransactionsByCampaignID(input GetCampaignTransactionsInput) ([]Transaction, error)
 }
 
 type service struct {
-	repository Repository
+	repository   Repository
+	campaignRepo campaign.Repository
 }
 
-func InstanceService(repository Repository) *service {
-	return &service{repository: repository}
+func InstanceService(repository Repository, campaignRepo campaign.Repository) *service {
+	return &service{repository: repository, campaignRepo: campaignRepo}
 }
 
 func (s *service) GetTransactionsByCampaignID(input GetCampaignTransactionsInput) ([]Transaction, error) {
-	transactions, err := s.repository.GetByCampaignID(input.ID)
+	
+	// get campaign
+	campaign, err := s.campaignRepo.FindByID(input.campaignID)
+	if err != nil {
+		return []Transaction{}, err
+	}
+
+	// check authorization
+	if campaign.UserID != input.User.ID {
+		return []Transaction{}, errors.New("not an owner of the campaign")
+	}
+	
+	transactions, err := s.repository.GetByCampaignID(input.campaignID)
 	if err != nil {
 		return transactions, err
 	}
